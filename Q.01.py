3370c7ed36ed055a857d0557bd1e1d06410badc5
Shubham Ambadas Nimase <shubhamnimase229@gmail.com>
2021-02-25 00:54:18 +0530

    Add files via upload

---
+++ Q.01.py
@@ -0,0 +1,12 @@
+
+#Write a program which contains one function named as Fun(). That function should display
+#“Hello from Fun” on console.
+
+def Fun():
+    print("Hello from Fun")
+
+def Main():
+    Fun()
+
+if __name__=="__main__":
+    Main()
